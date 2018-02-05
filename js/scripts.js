fa3a430a19e72a7ae780b6a8ee3b951472c78ca3
Adrian Slaby <adrianslaby@gmail.com>
2018-02-05 19:36:41 +0100

    add files

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var a = prompt ('podaj wartość a'),
+h = prompt ('podaj wartość h'),
+triangleArea = a*h/2;
+alert ('pole trójkąta wynosi' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
